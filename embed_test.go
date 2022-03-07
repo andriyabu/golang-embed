c87d719b5491e8ebb46a11e9f045f830f091bb61
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-03-08 05:38:49 +0700

    added TestByte function for binary file for example image, video etc

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package golangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed golang-logo.jpeg
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("new_golang_logo.svg", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
